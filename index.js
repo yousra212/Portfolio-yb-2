a649bb5afc518a4b94bab9f0a840f9dd19d0b18e
yousra212 <bakkaliyousra8@gmail.com>
2023-04-27 16:49:46 +0200

    Create Skills, Project and Contact sections

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ MenuBtn.addEventListener('click', function(event){
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark')
 })
+
+// Get all the links in the navigation menu
+const navLinks = document.querySelectorAll('nav ul li a');
+
+// For each link, add an event listener to scroll to the corresponding section
+navLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const section = document.querySelector(link.hash);
+    section.scrollIntoView({behavior: 'smooth'});
+  });
+});
